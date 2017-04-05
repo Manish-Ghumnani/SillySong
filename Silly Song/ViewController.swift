430f5acc879b9a78e6c42a9189536a7ddadfdfee
Manish Ghumnani <manishghumnani@LearniOSFreecoms-Mac.local>
2017-04-05 17:56:38 +0800

    first iteration

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+    }
 
+    @IBAction func displayLyrics(_ name:Any) {
+        
+        let bananaFanaTemplate = [
+            "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+            "Banana Fana Fo F<SHORT_NAME>",
+            "Me My Mo M<SHORT_NAME>",
+            "<FULL_NAME>"].joined(separator: "\n")
+
+        
+        if(!((nameField.text)?.isEmpty)!){
+            lyricsView.text = lyricsForName(bananaFanaTemplate, name: nameField.text!)
+        }
+    }
 }
+
+func shortName(_ Name: String) -> String {
+    var name = Name.lowercased()
+    let vowels:[Character] = ["a","e","i","o","u"]
+    
+    for char in name.characters {
+        if(!vowels.contains(char)){
+            name = name.replacingOccurrences(of: String(char), with: "")
+        }
+        else{
+            break
+        }
+    }
+    
+    return name
+}
+
+
+
+
+func lyricsForName(_ template:String, name: String) -> String {
+    let shortenedName = shortName(name)
+    var customTemplate = template.replacingOccurrences(of: "<FULL_NAME>", with: name)
+    
+    customTemplate =  customTemplate.replacingOccurrences(of: "<SHORT_NAME>", with: shortenedName)
+    
+    return customTemplate
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameField.resignFirstResponder() //on return - hide the keyboard and the 'editing did end' event ended
+        return false //to prevent further responses to return key
+    }
+}
+
 
